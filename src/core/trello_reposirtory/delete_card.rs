a2df25ca35cdcbca132dea0038c4defa48bf59dd
Lucas Oliveira <olivmath@protonmail.com>
2024-06-13 17:16:43 -0300

    add: delete all cards

--- src/core/trello_reposirtory/delete_card.rs
+++ src/core/trello_reposirtory/delete_card.rs
@@ -27,3 +27,57 @@ pub async fn delete_card(id: &str) {
     }
 }
 
+pub async fn delete_all_cards() {
+    let mut base_url = var("BASE_URL").expect("BASE_URL must be set");
+    let board_id = var("BOARD_ID").expect("BOARD_ID must be set");
+
+    base_url.push_str("/boards/");
+    base_url.push_str(&board_id);
+    base_url.push_str("/cards");
+
+    let response = get(&base_url).await;
+
+    match response {
+        Ok(r) => {
+            if r.status().is_success() {
+                let text = r
+                    .text()
+                    .await
+                    .unwrap_or_else(|_| "No response body".to_string());
+                match serde_json::from_str::<Vec<Card>>(&text) {
+                    Ok(cards) => {
+                        let mut tasks = vec![];
+
+                        for card in cards {
+                            println!("{}", card);
+                            let task = tokio::spawn(async move {
+                                delete_card(&card.id).await;
+                            });
+                            tasks.push(task);
+                        }
+
+                        for t in tasks {
+                            if let Err(e) = t.await {
+                                eprintln!("Erro Card Creation: {:?}", e);
+                            }
+                        }
+                        println!("✅ Deleted all Card");
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to parse JSON: {:?}", e);
+                        eprintln!("{}", text);
+                        panic!()
+                    }
+                }
+            } else {
+                eprintln!("Error: Received status code {}", r.status());
+                eprintln!("Response: {}", r.text().await.unwrap());
+            }
+        }
+        Err(e) => {
+            eprintln!("🚨 Get Card Error");
+            eprintln!("{:?}", e);
+            panic!()
+        }
+    }
+}
